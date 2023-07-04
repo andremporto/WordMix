079a3f49299e8cbffd973ad39eae836f86e9172e
André Porto <andreporto@me.com>
2023-07-04 06:59:35 -0300

    Initial Commit

---
+++ WordMix/WordMixApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordMixApp.swift
+//  WordMix
+//
+//  Created by André Porto on 04/07/23.
+//
+
+import SwiftUI
+
+@main
+struct WordMixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
